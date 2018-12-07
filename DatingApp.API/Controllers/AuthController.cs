286a6e9224597818c37dedff7a8efa788ee7edaf
Karan Bhamra <karan@karanbhamra.com>
2018-12-06 18:44:50 -0800

    Added register user functionality; TODO: username/password validation

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            this._repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegister)
+        {
+            // validate request placeholder
+
+            userForRegister.Username = userForRegister.Username.ToLower(); // make username consistent
+
+            if (await _repo.UserExists(userForRegister.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegister.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegister.Password);
+
+            return StatusCode(201);
+        }
+
+    }
+}
