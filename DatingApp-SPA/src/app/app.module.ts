ab2365f94628f911bfa976998a78d6d139e829b4
Karan Bhamra <karan@karanbhamra.com>
2018-12-08 15:08:05 -0800

    injecting angular service to get token from api

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from './_services/auth.service';
 @NgModule({
    declarations: [
       AppComponent,
@@ -16,7 +17,9 @@ import { FormsModule } from '@angular/forms';
       HttpClientModule,
       FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
