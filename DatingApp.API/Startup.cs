0abab862f0a163969c6b6af4abea802060b2ff7a
Karan Bhamra <karan@karanbhamra.com>
2018-12-10 13:01:17 -0800

    added dating repository inteface with implementation

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,8 +37,9 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
-
+            services.AddScoped<IDatingRepository, DatingRepository>();
             // add authentication as a service
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -56,7 +57,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -82,6 +83,8 @@ namespace DatingApp.API
                 // app.UseHsts();
             }
 
+            // un comment whenever we need to reseed
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             // app.UseHttpsRedirection();
